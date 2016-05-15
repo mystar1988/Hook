cc00b7fb997cb93db6268fe49d159a1a58e4ae1d
Anton <mystart1988@outlook.com>
2016-05-15 03:45:41 +0300

    Initial commit

---
+++ TextDisplay.h
@@ -0,0 +1,37 @@
+﻿#pragma once
+
+#include <string>
+#include <cmath>
+
+
+namespace Capture
+{
+	namespace Hook
+	{
+		class TextDisplay
+		{
+		private:
+			bool privateDisplay = false;
+			std::wstring privateText;
+			TimeSpan privateDuration;
+
+			long long _startTickCount = 0;
+
+		public:
+			TextDisplay();
+
+			/// <summary>
+			/// Must be called each frame
+			/// </summary>
+			void Frame();
+
+				const bool &getDisplay() const;
+				void setDisplay(const bool &value);
+				const std::wstring &getText() const;
+				void setText(const std::wstring &value);
+				const TimeSpan &getDuration() const;
+				void setDuration(const TimeSpan &value);
+			const float &getRemaining() const;
+		};
+	}
+}
